80bfff69c41b552c1bc6e85b67136b9ae23783e5
SAHI1SINGH <sahil9955817567@gmail.com>
2025-11-11 22:01:11 +0530

    Question number 3

---
+++ src/array/Q3.java
@@ -0,0 +1,10 @@
+package array;
+
+public class Q3 {
+		public static void main(String [] args) {
+			String [] name= {"alpha","beta","gama","ghost","makarov"};
+			for( int i=0;i<name.length;i++) {
+				System.out.println(name[i].length());
+			}
+		}
+}
