5629e81bc95794448f05a48e090122f6164a32f9
SAHI1SINGH <sahil9955817567@gmail.com>
2025-11-17 09:44:49 +0530

    updated question 15

--- src/array/Q15.java
+++ src/array/Q15.java
@@ -4,7 +4,7 @@ public class Q15 {
  public static void main(String[] args) {
 	 int [] nums = {10,20,30,40,50,60,70};
 	 int n=nums.length;
-	 int mid =n/2;
+	 int mid =(n/2)+1;
 	 for(int i=mid-1;i>=0;i--) {
 		 System.out.print(nums[i]+" ");
 	 }
